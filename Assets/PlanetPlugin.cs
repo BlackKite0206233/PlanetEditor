77a51454dda4801d82db3ad3bab1c2d9debf9da0
BlackKite0206233 <samuel991640@gmail.com>
2017-01-26 01:36:32 +0800

    add PlanetPlugin and Planet files

--- Assets/PlanetPlugin.cs
+++ Assets/PlanetPlugin.cs
@@ -1,16 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class PlanetPlugin : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [MenuItem("PlanetPlugin/add Planet")]
+    static void Start()
+    {
+        GameObject obj  = new GameObject("planet");
+        Planet     p    = obj.AddComponent<Planet>();
+    }
 }
