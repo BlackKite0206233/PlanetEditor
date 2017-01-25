90db39a761a5e387a178314c3ea024c542f0b2d8
BlackKite0206233 <samuel991640@gmail.com>
2017-01-26 01:53:47 +0800

    switch to V2

---
+++ Assets/PlanetPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PlanetPlugin : MonoBehaviour {
+
+    [MenuItem("PlanetPlugin/add Planet")]
+    static void Start()
+    {
+        GameObject obj  = new GameObject("planet");
+        Planet     p    = obj.AddComponent<Planet>();
+    }
+}
